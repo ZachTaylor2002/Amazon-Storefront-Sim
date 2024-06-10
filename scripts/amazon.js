a422829d6bb6b2cc1e8456a011668315ade10dc1
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-10 15:59:07 -0500

    Mistake in word

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -16,7 +16,7 @@ products.forEach((product) => {
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars *10 }.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.coount}
+              ${product.rating.count}
             </div>
           </div>
 
