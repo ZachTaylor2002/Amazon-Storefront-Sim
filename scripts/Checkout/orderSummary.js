8b2266e04d5e8d94614e412317c12f7567e35137
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-11 10:51:18 -0500

    Changes

--- scripts/Checkout/orderSummary.js
+++ scripts/Checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeilveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 const today = dayjs();
 const deliverydate = today.add(7,'days');
@@ -129,6 +130,9 @@ export function renderOrderSummary(){
     const container =  document.querySelector(`.js-cart-item-container-${productId}`);
 
     container.remove();
+
+
+    renderPaymentSummary();
         
     });
     });
@@ -139,6 +143,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 
