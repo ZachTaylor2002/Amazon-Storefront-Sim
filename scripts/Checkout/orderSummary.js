5ad7119d990ca897f1651bd4aea096ccefe66b34
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-13 09:51:42 -0500

    Cleaning up and making code neater

--- scripts/Checkout/orderSummary.js
+++ scripts/Checkout/orderSummary.js
@@ -48,9 +48,7 @@ export function renderOrderSummary() {
                                 <span>
                                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                                 </span>
-                                <span class="update-quantity-link link-primary">
-                                    Update
-                                </span>
+                                
                                 <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                                     Delete
                                 </span>
